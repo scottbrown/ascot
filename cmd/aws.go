f63eca8cfe41a55e120a314b914885009493510f
Scott Brown <mail@typicalrunt.me>
2022-10-03 09:38:31 -0600

    Refactoring common functions

---
+++ cmd/aws.go
@@ -0,0 +1,49 @@
+package cmd
+
+import (
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
+
+	"context"
+)
+
+func getAWSConfig(region string, profile string) (aws.Config, error) {
+	var cfg aws.Config
+	var err error
+
+	if profile != "" {
+		cfg, err = config.LoadDefaultConfig(context.TODO(),
+			config.WithRegion(region),
+			config.WithSharedConfigProfile(profile),
+		)
+		if err != nil {
+			return aws.Config{}, err
+		}
+	} else {
+		// use the default profile
+		cfg, err = config.LoadDefaultConfig(context.TODO(),
+			config.WithRegion(region),
+		)
+		if err != nil {
+			return aws.Config{}, err
+		}
+	}
+
+	return cfg, nil
+}
+
+func getAllRegions(cfg aws.Config) ([]types.Region, error) {
+	client := ec2.NewFromConfig(cfg)
+
+	resp, err := client.DescribeRegions(context.TODO(),
+		&ec2.DescribeRegionsInput{},
+	)
+
+	if err != nil {
+		return []types.Region{}, err
+	}
+
+	return resp.Regions, nil
+}
